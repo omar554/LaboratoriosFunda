76850d331c178fc885f0f5e7ea56dcbc3121e06f
omar554 <62614708+omar554@users.noreply.github.com>
2020-05-07 14:43:08 -0600

    Add files via upload

---
+++ LabosFunda/prue1.cpp
@@ -0,0 +1,14 @@
+#include "iostream"
+
+using namespace std;
+
+int main(){
+    int num1,num2;
+    cin>>num1>>num2;
+
+    int suma=num1+num2;
+
+    string nombre-"omar";
+    cout<<nombre + ",el resultado es :"<< suma;
+    return 0;
+}
